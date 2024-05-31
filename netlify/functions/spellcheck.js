01ba88ae70ff531a4ade2e605bf6fa3a5d1a6b87
srinathvv-dev <srinathvv.dev@gmail.com>
2024-05-31 19:00:55 +0530

    change

--- netlify/functions/spellcheck.js
+++ netlify/functions/spellcheck.js
@@ -1,7 +1,7 @@
-exports.handler = async (event, context) => {
+export async function handler(event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Hello from Netlify function!' })
     };
-  };
+  }
   
